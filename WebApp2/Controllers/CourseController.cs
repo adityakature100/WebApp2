b61129dec894164b87e12669d2133cb6e9a72685
Aditya Kature <v-akature@microsoft.com>
2023-02-27 11:27:39 +0530

    Add project files.

---
+++ WebApp2/Controllers/CourseController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebApp2.Models;
+using WebApp2.Services;
+
+namespace WebApp2.Controllers
+{
+    public class CourseController : Controller
+    {
+        private readonly CourseService courseService;
+        private readonly IConfiguration configuration;
+
+        public CourseController(CourseService courseService, IConfiguration configuration)
+        {
+            this.courseService = courseService;
+            this.configuration = configuration;
+        }
+        public IActionResult Index()
+        {
+            IEnumerable<Course> course_list = courseService.GetCourses(configuration.GetConnectionString("SQLConnection"));
+            return View(course_list);
+        }
+    }
+}
---
+++ WebApp2/Services/CourseService.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Threading.Tasks;
+using WebApp2.Models;
+
+namespace WebApp2.Services
+{
+    public class CourseService
+    {
+        private SqlConnection GetConnection(string connectionString)
+        {
+            return new SqlConnection(connectionString);
+        }
+
+        public IEnumerable<Course> GetCourses(string connectionString)
+        {
+            List<Course> courses = new List<Course>();
+            string sql = "select CourseId,CourseName,Rating from Course";
+            SqlConnection sqlConnection = GetConnection(connectionString);
+            sqlConnection.Open();
+            SqlCommand sqlCommand = new SqlCommand(sql, sqlConnection);
+            using (SqlDataReader reader = sqlCommand.ExecuteReader())
+            {
+                while (reader.Read())
+                {
+                    Course course = new Course()
+                    {
+                        CourseId = reader.GetInt32(0),
+                        CourseName = reader.GetString(1),
+                        Rating = reader.GetDecimal(2)
+                    };
+                    courses.Add(course);
+                }
+            }
+            sqlConnection.Close();
+            return courses;
+        }
+    }
+}
